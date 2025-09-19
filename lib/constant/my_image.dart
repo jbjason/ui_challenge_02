1e6ac13c221f530961c20e8134ec2762577fb28e
jubayer040 <62088928+jbjason@users.noreply.github.com>
2025-09-19 15:36:09 +0600

    project setup done

---
+++ lib/constant/my_image.dart
@@ -0,0 +1,3 @@
+abstract class MyImage {
+  static const String galaxyImg = "assets/galaxy.jpg";
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:ui_challenge_02/constant/my_color.dart';
+import 'package:ui_challenge_02/constant/my_image.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(),
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Jb Jason's Transaction",
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  Text(
+                    "Flutter Dev",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    height: size.height * .3,
+                    width: size.width,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Recommend"),
+                            Icon(Icons.arrow_back_ios_new)
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          height: size.height * .25,
+                          child: Row(children: [
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12),
+                                  color: Colors.purple[50],
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 20),
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12),
+                                  color: Colors.deepPurple[50],
+                                ),
+                              ),
+                            ),
+                          ]),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 20,
+            right: 20,
+            top: size.height * .15,
+            height: size.height * .45,
+            child: Container(
+              constraints: BoxConstraints.expand(),
+              decoration: BoxDecoration(
+                color: MyColor.primaryColor,
+                borderRadius: BorderRadius.circular(17),
+                image: DecorationImage(
+                  image: AssetImage(MyImage.galaxyImg),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

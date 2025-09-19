45e4d9828d51da90e00c702632550092651bcda2
jubayer040 <62088928+jbjason@users.noreply.github.com>
2025-09-19 14:58:53 +0600

    project configure done

---
+++ lib/constant/my_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class MyColor {
+  static const Color primaryColor = Color(0xFF1A237E);
+  static const Color whiteColor = Color(0xFFEEEEEE);
+
+}
